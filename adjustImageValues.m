9c53a523ff0b13c649d2c78b2dac6614b69ff01a
Martin Hag <martin-hag@hotmail.com>
2019-11-18 09:46:48 +0100

    Pixel sorting done

--- adjustImageValues.m
+++ adjustImageValues.m
@@ -4,7 +4,19 @@ if isa(im, 'uint8')|isa(im, 'uint16')
     im = im2double(im);
 end
 
+% Check for light interferance.
+
+%% Step 1: Check if top 5% of all pixels is more than a value(1000).
+SortedPixelValues = reshape(im,[],size(im,3),1);
+SortedPixelValues = sort(SortedPixelValues);
+
+MaxPixelValue = max(SortedPixelValues(:)');
+
+
+
+
 % Make image grayscale
 im = rgb2gray(im);
+
 
 out = im;
