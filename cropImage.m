735028e3a99601cdbf60445ba785d0b69911f533
Martin Hag <martin-hag@hotmail.com>
2019-11-28 10:43:59 +0100

    Add functions for scaling, rotation and crop image

---
+++ cropImage.m
@@ -0,0 +1,10 @@
+function out = cropImage(im, mid, height, width)
+
+h = height/2;
+w = width/2;
+x = mid(1)-w; % Top left x-pos
+y = mid(2)-h; % Top left y-pos
+
+out = imcrop(im,[x y width height]);
+
+
